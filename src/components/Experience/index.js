b9afa0b96ebfa65846dfe1c6f637068c651ecbdb
Wellington2708 <wellingtondevelopercontact@gmail.com>
2024-09-04 21:38:44 +0100

    refacturing to new component jobExperience

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,23 +1,104 @@
-import ExperienceCSS from './Experience.module.css'
+import JobExperience from '../JobExperience'
+function Experience() {
+    const bodum = {
+        subtitle: "Production Operator - Bodum - Gafanha da Encarnação - Portugal",
+        period: "Date: 2024-09-02 to Today",
+        tasks: [
+            "Production.",
+            "Stock organization.",
+            "Cleaning of the work area"
+        ]
+    };
 
-function Experience({subtitle, period, task1, task2, task3, task4}){
-    return(
-        
+    const tensai = {
+        subtitle: "Production Operator - Tensai - Estarreja - Portugal",
+        period: "Date: 2024-01-09 to 2024-08-14",
+        tasks: [
+            "Freezer assembly",
+            "Production line.",
+            "Stock organization."
+        ]
+    };
+
+    const bruma = {
+        subtitle: "Production Operator - Bruma - Famalicão - Portugal",
+        period: "Date: 2022-11-16 to 2023-11-23",
+        tasks: [
+            "Responsible for dismantling parts.",
+            "Demand control.",
+            "Production.",
+            "Stock organization."
+        ]
+    };
+
+    const natuflores = {
+        subtitle: "Dispatch - Natuflores - Goiânia - BR",
+        period: "Date: 2021-02-16 to 2022-03-01",
+        tasks: [
+            "Responsible for picking and sending orders to customers.",
+            "Logistics control.",
+            "Production assistant.",
+            "Stock organization."
+        ]
+    };
+
+    const galpTrofa = {
+        subtitle: "Front attendant/Supplier - GALP TROFA - Portugal",
+        period: "Date: 2020-01-01 to 2020-06-15",
+        tasks: [
+            "Responsible for fueling customer vehicles.",
+            "Washing and drying.",
+            "Inventory supply.",
+            "Stock organization and cleaner."
+        ]
+    };
+
+    const decorGoiania = {
+        subtitle: "Production Assistant - DECOR - Goiânia - BR",
+        period: "Date: 2012-10-01 to 2013-07-01 - Goiânia - BR",
+        tasks: [
+            "Production of construction parts and materials.",
+            "Inventory control, sending orders to the customer.",
+            "Production operator.",
+            "Stock organization."
+        ]
+    };
+    const cdiGoiania = {
+        subtitle: "Administrative Assistant – Nuclear CDI - Goiânia",
+        period: "Date: 2014-03-08 to 2017-07-07 - Goiânia - BR",
+        tasks: [
+            "Responsible for charging for services provided.",
+            "Invoice Control.",
+            "Typist.",
+            "Accounts payable control"
+        ]
+    };
+
+    const cebramenGoiania = {
+        subtitle: "Administrative Assistant – CEBRAMEN - Goiânia - BR",
+        period: "Date: 2018-03-08 to 2019-07-07 - Goiânia - BR ",
+        tasks: [
+            "Responsible for charging for services provided.",
+            "Invoice Control.",
+            "Typist.",
+            "Accounts payable control"
+        ]
+    };
+
+    return (
         <>
-                <span className={ExperienceCSS.professional__experience}>
-                    <h2>{subtitle}</h2>
-                    <p className={ExperienceCSS.period}>{period}</p>
-                </span>
-                <ul>
-                    <li className={ExperienceCSS.item}>{task1}</li>
-                    <li className={ExperienceCSS.item}>{task2}</li>
-                    <li className={ExperienceCSS.item}>{task3}</li>
-                    <li className={ExperienceCSS.item}>{task4}</li>
+            <JobExperience {...bodum} />
+            <JobExperience {...tensai} />
+            <JobExperience {...bruma} />
+            <JobExperience {...natuflores} />
+            <JobExperience {...galpTrofa} />
+            <JobExperience {...cebramenGoiania} />
+            <JobExperience {...cdiGoiania} />
+            <JobExperience {...decorGoiania} />
 
-                </ul>
+
         </>
-      
-    )
+    );
 }
 
 export default Experience
---
+++ src/components/JobExperience/index.js
@@ -0,0 +1,17 @@
+import ExperienceCSS from '../Experience/Experience.module.css'
+
+function JobsExperience({ subtitle, period, tasks }) {
+    return(
+    <div className={ExperienceCSS.professional__experience}>
+        <h2>{subtitle}</h2>
+        <p className={ExperienceCSS.period}>{period}</p>
+        <ul>
+        {tasks.map((task, index) => (
+            <li key={index} className={ExperienceCSS.item}>{task}</li>
+        ))}
+
+        </ul>
+    </div> )
+};
+
+export default JobsExperience;
